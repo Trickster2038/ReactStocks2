51c5e1cb0ce5a6875d567ff1e988ee26b127230c
Astakhov Sergey <fzastahov@gmail.com>
2022-06-24 20:37:25 +0300

    i18n init

---
+++ front/stoxxx2/src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+import { useTranslation } from 'react-i18next';
+import LanguageSelect from "./languageSelect";
+
+function App() {
+  const { t } = useTranslation();
+
+  return (
+    <div>
+      <div className="App">
+        <header className="App-header">
+
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <div className="language-select">
+            <LanguageSelect />
+          </div>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <h1>{t('caption0')}</h1>
+        </header>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ front/stoxxx2/src/i18n.js
@@ -0,0 +1,42 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import Backend from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
+
+import translationEN from "./locales/en/translation.json";
+import translationRU from "./locales/ru/translation.json";
+
+const fallbackLng = ["en"];
+const availableLanguages = ["en", "ru"];
+
+const resources = {
+  en: {
+    translation: translationEN
+  },
+  ru: {
+    translation: translationRU
+  }
+};
+
+i18n
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng,
+
+    detection: {
+      checkWhitelist: true
+    },
+
+    debug: true,
+
+    whitelist: availableLanguages,
+
+    interpolation: {
+      escapeValue: false
+    }
+  });
+
+export default i18n;
