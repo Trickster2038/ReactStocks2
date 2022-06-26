9f04ace24af9eda89a9b3d102a56d55861e7fdf2
Astakhov Sergey <fzastahov@gmail.com>
2022-06-26 21:59:33 +0300

    profile card proto

--- frontend/stoxxx2/src/components/statsPage.js
+++ frontend/stoxxx2/src/components/statsPage.js
@@ -5,16 +5,14 @@ import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
 
-import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
-import FormHelperText from '@mui/material/FormHelperText';
-import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 
 import IndicatorsGrid from "./indicatorsGrid";
 import TradingViewWidget, { Themes } from "react-tradingview-widget";
 
 import { useTranslation } from 'react-i18next';
+import { SymbolInfo, FundamentalData, CompanyProfile } from "react-tradingview-embed";
 
 export default function StatsPage() {
     const [tabValue, setTabValue] = React.useState('1');
@@ -35,37 +33,74 @@ export default function StatsPage() {
     const symb = urlParams.get('symb')
     const tag = urlParams.get('tag')
 
+    // console.log("banner")
+
     return (
         <Box sx={{ width: '100%', typography: 'body1' }}>
             <TabContext value={tabValue}>
                 <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
                     <TabList onChange={handleChangeTabs} aria-label="lab API tabs example">
                         <Tab label={t("overview")} value="1" />
-                        <Tab label={t("tech-analysis")} value="2" />
+                        <Tab label={t("company-profile")} value="2" />
+                        <Tab label={t("tech-analysis")} value="3" />
                     </TabList>
                 </Box>
                 <TabPanel value="1">
+
                     <div id="trading-view-frame">
-                        <TradingViewWidget
-                            symbol={symb}
-                            theme={Themes.LIGHT}
-                            locale={i18n.language}
-                            autosize
-                        />
+                        <div class="trading-view-card">
+                            <SymbolInfo
+                                widgetProps={{
+                                    "colorTheme": "Light",
+                                    "symbol": symb,
+                                    "locale": i18n.language,
+                                    "width": "100%"
+                                }}
+                            />
+                        </div>
+                        <div class="trading-view-card">
+                            <TradingViewWidget
+                                symbol={symb}
+                                theme={Themes.LIGHT}
+                                locale={i18n.language}
+                                autosize
+                            />
+                        </div>
+                        <div class="trading-view-card">
+                            <FundamentalData
+                                widgetProps={{
+                                    "colorTheme": "Light",
+                                    "symbol": symb,
+                                    "locale": i18n.language,
+                                    "width": "100%"
+                                }}
+                            />
+                        </div>
                     </div>
                 </TabPanel>
                 <TabPanel value="2">
                     <div>
-                            <Select
-                                value={interval}
-                                onChange={handleChangeSelect}
-                                defaultValue={"daily"}
-                                inputProps={{ 'aria-label': 'Without label' }}
-                            >
-                                <MenuItem value={"daily"}>Daily</MenuItem>
-                                <MenuItem value={"weekly"}>Weekly</MenuItem>
-                                <MenuItem value={"monthly"}>Monthly</MenuItem>
-                            </Select>
+                        <CompanyProfile
+                            widgetProps={{
+                                "colorTheme": "Light",
+                                "symbol": symb,
+                                "locale": i18n.language,
+                                "width": "100%"
+                            }} />
+                    </div>
+                </TabPanel>
+                <TabPanel value="3">
+                    <div>
+                        <Select
+                            value={interval}
+                            onChange={handleChangeSelect}
+                            defaultValue={"daily"}
+                            inputProps={{ 'aria-label': 'Without label' }}
+                        >
+                            <MenuItem value={"daily"}>Daily</MenuItem>
+                            <MenuItem value={"weekly"}>Weekly</MenuItem>
+                            <MenuItem value={"monthly"}>Monthly</MenuItem>
+                        </Select>
                     </div>
                     <div>
                         {/* <p>{interval}</p> */}
